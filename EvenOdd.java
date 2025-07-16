1390cd6c4d826776bef4cb6f956c2063f1e89888
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-07-16 18:54:55 +0530

    finding Number even or Odd and Positive or Negative

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n=-5;
+        if(n>0){
+            System.out.println("Positive");
+        }else{
+            System.out.println("negative");
+        }
+
+        if(n%2==0){
+                System.out.println("Even");
+        }else{
+                System.out.println("Odd");
+        }
+    }
+}
