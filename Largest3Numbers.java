099685bc3ae8c50cbbadf9206460c8ff59c0b80f
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-07-16 14:33:33 +0530

    Largest of 3 Numbers

---
+++ Largest3Numbers.java
@@ -0,0 +1,13 @@
+public class Largest3Numbers {
+    public static void main(String[] args) {
+        int a=5;int b=2;int c=46;
+
+        if(a>b && a>c){
+            System.out.println("a is greater "+a);
+        }else if(b>c){
+            System.out.println("b is grater "+b);
+        }else{
+            System.out.println("c is greater "+c);
+        }
+    }
+}
