b46c2ac406f1ecb01d19fbf020148dfa6dd7bd85
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-07-24 13:57:42 +0530

    creationg class intilizing object

---
+++ classExample.java
@@ -0,0 +1,9 @@
+public class classExample {
+    int aa=25;
+    int b=65;
+
+    public static void main(String[] args) {
+        classExample t=new classExample();
+        System.out.println(t.aa);
+    }
+}
