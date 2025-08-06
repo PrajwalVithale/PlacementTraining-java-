67221da656d2554ba70f92e74e2c41c5524c16bb
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-08-06 09:20:06 +0530

    Printing the elements from mid of linkedlist

---
+++ PrintingElementsFromMidUsingJCF.java
@@ -0,0 +1,22 @@
+import java.util.*;
+//using JCF
+public class PrintingElementsFromMidUsingJCF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> list = new LinkedList<>();
+
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        // Calculate mid
+        int mid = list.size() / 2;
+
+        for (int i = mid; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+
+        sc.close();
+    }
+}
