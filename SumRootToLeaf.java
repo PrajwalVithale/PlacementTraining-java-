e07e9afee7f666ff3a5cf96e6fed4cd2224163b4
PrajwalVithale <PRAJWAL.20221CSE0544@presidencyuniversity.in>
2025-08-13 10:15:38 +0530

    Sum Of Leaf Node of A Binary Tree

--- SumRootToLeaf.java
+++ SumRootToLeaf.java
@@ -9,7 +9,6 @@ class Node {
 }
 
 public class SumRootToLeaf {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //int n = Integer.parseInt(sc.nextLine().trim()); // Number of nodes
